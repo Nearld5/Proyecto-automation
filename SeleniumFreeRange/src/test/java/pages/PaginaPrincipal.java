823c3fa8517fae70f8e4b593984943d82a507ec9
Nelson Alarcon <nalarcon1018@hotmail.com>
2024-10-17 10:50:55 -0500

    Cambios a archivo fature (Scenario Outline para poder enlistar barra de navegacion) y tener conjunto de opciiones de navigation bar, se coloca xpath dinamico

--- SeleniumFreeRange/src/test/java/pages/PaginaPrincipal.java
+++ SeleniumFreeRange/src/test/java/pages/PaginaPrincipal.java
@@ -2,7 +2,7 @@ package pages;
  
 public class PaginaPrincipal extends BasePage {
  
-    private String searchButton = "//*[@id=\"page_section_48252437\"]/div/section/div[2]";
+    private String sectionLink = "//a[normalize-space()='%s' and @href]";
  
     public PaginaPrincipal() {
         super(driver);
@@ -11,7 +11,13 @@ public class PaginaPrincipal extends BasePage {
     // Método para navegar a www.freerangetesters.com
     public void navigateToFreeRangeTesters() {
         navigateTo("https://www.freerangetesters.com");
-        clickElement(searchButton);
+ 
+    }
+ 
+    public void clickOnSectionNavigationBar(String section) {
+        // Reemplaza el marcador de posición en sectionLink con el nombre
+        String xpathSection = String.format(sectionLink, section);
+        clickElement(xpathSection);
     }
  
 }
--- SeleniumFreeRange/src/test/resources/FreeRangeNavegacion.feature
+++ SeleniumFreeRange/src/test/resources/FreeRangeNavegacion.feature
@@ -2,11 +2,14 @@ Feature: Navigation bar
     To see the subpages
     Without logging in
     I can click the navigation bar links
- 
-Scenario: I can access the subpages through the navigation bar
-    Given I navigate to www.freerangetesters.com
-    
-    
-    
 
-    
+    Scenario Outline: I can access the subpages through the navigation bar
+        Given I navigate to www.freerangetesters.com
+        When I go to <section> using the navigation bar
+        Examples:
+            | section   |
+            | Cursos    |
+            | Recursos  |
+            | Udemy     |
+            | Mentorías |
+            | Blog      |
