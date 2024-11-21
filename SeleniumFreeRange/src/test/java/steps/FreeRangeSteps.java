9029a9d2d43762cb9b58cf0a4ec1202895e2c96d
Nelson Alarcon <nalarcon1018@hotmail.com>
2024-11-20 21:23:53 -0500

    Se agrega background, se elimina carpeta proyecto auto, se agregan tags y se incorporan nuevas funcionalidades para los steps, gramatica Gerking

--- SeleniumFreeRange/src/test/java/steps/FreeRangeSteps.java
+++ SeleniumFreeRange/src/test/java/steps/FreeRangeSteps.java
@@ -3,6 +3,8 @@ package steps;
 import java.util.Arrays;
 import java.util.List;
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
 import io.cucumber.java.en.*;
 import pages.PaginaCursos;
 import pages.PaginaPrincipal;
@@ -14,6 +16,11 @@ public class FreeRangeSteps {
     PaginaCursos cursosPage = new PaginaCursos();
     PaginaRegistro registro = new PaginaRegistro();
 
+//Instancia para usar softAssert de TestNG
+
+SoftAssert softAs = new SoftAssert();
+
+
     @Given("I navigate to www.freerangetesters.com")
     public void iNavigateToFRT() {
         landingPage.navigateToFreeRangeTesters();
@@ -24,17 +31,17 @@ public class FreeRangeSteps {
         landingPage.clickOnSectionNavigationBar(section);
     }
 
-    @When("I select elegir plan")
+    @When("^(?:I|The user|The client) selects? elegir plan$")
     public void selectElegirPlan() {
         landingPage.ClickOnElegirPlanButton();
     }
 
-    @And("Select Introducción al Testing")
+    @And("^(?:I|The user|The client) Selects? Introducción al Testing$")
     public void navigatetoIntro(){
         cursosPage.clickFundamentosTestingLink();
     }
 
-    @Then("I can validate the options in the checkout page") 
+    @Then("^(?:I|The user|The client) can validate the options in the checkout page$") 
     public void returnPlanInputsValues(){
       List<String> lista = registro.returnPlanInputsValues();
       List<String> listaEsperada = Arrays.asList("$25", "6 pagos mensuales de $4.50");
@@ -42,4 +49,51 @@ public class FreeRangeSteps {
     Assert.assertEquals(lista, listaEsperada);
 
     }
+
+
+        //EJEMPLOS ASSERTIONS MAS COMUNES
+
+    public void ejemplosAssertions(){
+        String palabraEsperada = "Pepe";
+        String palabraEncontrada = "Papa";
+
+        //Verificar que dos valores son iguales 
+
+        Assert.assertEquals(palabraEsperada, palabraEncontrada);
+
+        //Verificar que dos valores son iguales, pero con numeros
+        int numeroloco = 1;
+        int numeroloco2 = 2;
+        Assert.assertEquals(numeroloco, numeroloco2);
+
+        // Verificar que dos valores NO son iguales
+
+        Assert.assertNotEquals(palabraEsperada, palabraEncontrada);
+
+        //Verificar que una condición es verdadera
+
+        Assert.assertTrue(palabraEncontrada.contains(palabraEsperada));
+
+        //Verificar que una condición es falsa
+
+        Assert.assertFalse(palabraEncontrada.contains(palabraEsperada));
+
+        //Verifica que el objeto sea null.
+
+        Assert.assertNull(numeroloco);
+
+        //Verifica que el objeto NO sea null.
+
+        Assert.assertNotNull(numeroloco);
+
+        //soft Assertions: No detienen la ejecucion al fallar. Ideal para verificar muchas cosas pequeñas a la vez
+        softAs.assertEquals(palabraEsperada, palabraEncontrada);
+        softAs.assertTrue(palabraEncontrada.contains(palabraEsperada));
+        softAs.assertNotEquals(palabraEncontrada, palabraEsperada);
+
+        softAs.assertAll();
+    }
+
+  
+
 }
--- SeleniumFreeRange/src/test/resources/FreeRangeNavegacion.feature
+++ SeleniumFreeRange/src/test/resources/FreeRangeNavegacion.feature
@@ -1,27 +1,33 @@
+@Navigation
 Feature: Navigation bar
     To see the subpages
     Without logging in
     I can click the navigation bar links
+ 
+Background: I am on the Free Range Testers web without logging in.
+  Given I navigate to www.freerangetesters.com
 
-    # Scenario Outline: I can access the subpages through the navigation bar
-    #     Given I navigate to www.freerangetesters.com
-    #     When I go to <section> using the navigation bar
-    #     Examples:
-    #         | section   |
-    #         | Cursos    |
-    #         | Recursos  |
-    #         | Udemy     |
-    #         | Mentorías |
-    #         | Blog      |
-     
-        # Scenario: Courses are presented correctly to potential customers
-        # Given I navigate to www.freerangetesters.com
-        # When I go to Cursos using the navigation bar
-        # And Select Introducción al Testing
-        
-        Scenario: Users can select a plan when signing up
-        Given I navigate to www.freerangetesters.com
+
+    Scenario Outline: I can access the subpages through the navigation bar
+    #El Given de este scenario esta dado por el Backgroudnd
+        When I go to <section> using the navigation bar
+        Examples:
+            | section   |
+            | Cursos    |
+            | Recursos  |
+            | Udemy     |
+            | Mentorías |
+            | Blog      |
+     @Courses
+        Scenario: Courses are presented correctly to potential customers
+        #El Given de este scenario esta dado por el Backgroudnd
         When I go to Cursos using the navigation bar
-        And Select Introducción al Testing
-        When I select elegir plan
-        Then I can validate the options in the checkout page
+        And  I Select Introducción al Testing
+
+        @Plans
+        Scenario: Users can select a plan when signing up
+        #El Given de este scenario esta dado por el Backgroudnd
+        When I go to Cursos using the navigation bar
+        And I Select Introducción al Testing
+        When The client selects elegir plan
+        Then The client can validate the options in the checkout page
